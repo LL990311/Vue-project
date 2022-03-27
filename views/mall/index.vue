1ec1d7bd56de232405c66d91cf6436719b69dbe9
LL990311 <77871964+LL990311@users.noreply.github.com>
2022-03-27 14:32:19 +0800

    Add files via upload

---
+++ views/mall/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name: 'mall',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
